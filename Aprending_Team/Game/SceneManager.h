f2511f3d33eccc61ad265534255d311fd56cfcb8
Pol Vazquez Porcar <60131834+AMADE128@users.noreply.github.com>
2021-01-22 11:27:42 +0100

    Added Fade To Black

---
+++ Aprending_Team/Game/SceneManager.h
@@ -0,0 +1,88 @@
+#ifndef __SCENEMANAGER_H__
+#define __SCENEMANAGER_H__
+
+#include "Source/Module.h"
+#include "SceneControl.h"
+#include "Source/Window.h"
+
+class Input;
+class Render;
+class Textures;
+
+class SceneLogo;
+class SceneIntro;
+class Scene;
+class SceneWin;
+class SceneLose;
+
+class SceneManager : public Module
+{
+public:
+
+	SceneManager(Input* input, Render* render, Textures* tex);
+
+	// Destructor
+	virtual ~SceneManager();
+
+	// Called before render is available
+	bool Awake();
+
+	// Called before the first frame
+	bool Start();
+
+	// Called before all Updates
+	bool PreUpdate();
+
+	// Called each loop iteration
+	bool Update(float dt);
+
+	// Called before all Updates
+	bool PostUpdate();
+
+	// Add Scenes
+	void AddScene(SceneControl* scene, bool active);
+
+	// Return Pause
+	bool GetIsPause() { return pause; };
+	void SetPause(bool statePause) { pause = statePause; };
+
+	int GetGuiFont() { return guiFont; };
+
+	// Called before quitting
+	bool CleanUp();
+
+	bool GetViewCollisions() { return viewCollisions; };
+
+private:
+
+	Input* input;
+	Render* render;
+	Textures* tex;
+
+	SceneControl* current;
+	SceneControl* next;
+
+	// Required variables to manage screen transitions (fade-in, fade-out)
+	bool onTransition;
+	bool fadeOutCompleted;
+	float transitionAlpha;
+	bool pause = false;
+	List<SceneControl*> scenes;
+
+	bool viewCollisions = false;
+
+public:
+	SceneLogo* sceneLogo;
+	SceneIntro* sceneIntro;
+	Scene* scene;
+	SceneWin* sceneWin;
+	SceneLose* sceneLose;
+
+	SceneControl* sceneControl;
+
+	int lastLevel = 0;
+	int guiFont;
+};
+
+#endif // __SCENEMANAGER_H__
+
